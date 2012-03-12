6f85dbeaf61adbc5cf96491554e6c855ae47b51a
Chad Arimura <carimura@gmail.com>
2012-03-11 22:36:48 -0700

    - new loggly msg

--- ruby/loggly_logging/loggly_worker.rb
+++ ruby/loggly_logging/loggly_worker.rb
@@ -1,14 +1,12 @@
-# bump......
 class LogglyWorker < IronWorker::Base
-
   merge_gem 'logglier'
 
-  attr_accessor :loggly_key, :i
+  attr_accessor :loggly_key
 
   def run
     puts "I'm going to start logging to loggly... right... now!"
     log = Logglier.new("https://logs.loggly.com/inputs/#{loggly_key}")
-    log.info("I am \##{i}. If we knew what we were doing, it wouldn't be called research, would it?")
 
+    log.info("I am now logging to Loggly!")
   end
 end
