f1c6425b714fad50d3998288c057a9b4c577afec
Travis Reeder <treeder@gmail.com>
2012-04-11 00:35:13 -0700

    Update ruby_ng/github_to_hipchat_webhook_worker/github_to_hipchat_webhook_worker.rb

--- ruby_ng/github_to_hipchat_webhook_worker/github_to_hipchat_webhook_worker.rb
+++ ruby_ng/github_to_hipchat_webhook_worker/github_to_hipchat_webhook_worker.rb
@@ -15,7 +15,7 @@ webhook_config = YAML.load_file('webhook_config.yml')
 log "webhook_config: #{webhook_config.inspect}"
 
 hipchat = HipChat::API.new(webhook_config['hipchat']['api_key'])
-
+# Go through each commit and post a message to the chat room
 parsed['commits'].each do |c|
   log hipchat.rooms_message(webhook_config['hipchat']['room'], 'WebhookWorkerNG', "Rev: <a href=\"#{c['url']}\">#{c['id'][0,9]}</a> - #{c['message']}", true).body
 end
