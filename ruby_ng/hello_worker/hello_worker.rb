5bc6ea613eed16dcef5d676b7520ea3c76c27b6c
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-04-15 17:44:46 +0300

    Replaced log by puts.

--- ruby_ng/hello_worker/hello_worker.rb
+++ ruby_ng/hello_worker/hello_worker.rb
@@ -1,8 +1,8 @@
 # worker code is common ruby code
 # note that 'log' is just using 'puts' so any of your logging code will work
 
-log "Starting HelloWorker at #{Time.now}"
-log "We got following params #{params}"
-log "Simulating hard work for 10 seconds..."
+puts "Starting HelloWorker at #{Time.now}"
+puts "We got following params #{params}"
+puts "Simulating hard work for 10 seconds..."
 sleep 10
-log "Done running HelloWorker at #{Time.now}"
+puts "Done running HelloWorker at #{Time.now}"
--- ruby_ng/master_slave/master_worker.rb
+++ ruby_ng/master_slave/master_worker.rb
@@ -2,10 +2,10 @@ require 'iron_worker_ng'
 
 client = IronWorkerNG::Client.new(:token => iron_io_token, :project_id => iron_io_project_id) # they are magically available inside worker
 
-log "Queueing slave workers..."
+puts "Queueing slave workers..."
 slaves = params['slaves']
 slaves.keys.each do |slave|
-  log "Queueing #{slave} with params #{slaves[slave].inspect}"
+  puts "Queueing #{slave} with params #{slaves[slave].inspect}"
   client.tasks.create('SlaveWorker', slaves[slave])
 end
-log 'Done'
+puts 'Done'
