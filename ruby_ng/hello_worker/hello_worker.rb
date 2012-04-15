ac402961a746b948243c390ae15e5fc25bef40b9
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-04-15 17:45:26 +0300

    Replaced log by puts.

--- ruby_ng/hello_worker/hello_worker.rb
+++ ruby_ng/hello_worker/hello_worker.rb
@@ -1,5 +1,4 @@
 # worker code is common ruby code
-# note that 'log' is just using 'puts' so any of your logging code will work
 
 puts "Starting HelloWorker at #{Time.now}"
 puts "We got following params #{params}"
