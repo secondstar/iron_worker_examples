db1b461988b333194fdf7dbb7b767f049024d91c
thousandsofthem <thousandsofthem@gmail.com>
2012-04-18 14:16:02 +0300

    simplified uploading process

--- php/Airbrake.php
+++ php/Airbrake.php
@@ -6,11 +6,7 @@ $name = "airbrake-php";
 $iw = new IronWorker('config.ini');
 $iw->debug_enabled = true;
 
-$zipName = "code/$name.zip";
-
-$zipFile = IronWorker::zipDirectory(dirname(__FILE__)."/workers/airbrake", $zipName, true);
-
-$res = $iw->postCode('airbrake.php', $zipName, $name);
+$iw->upload(dirname(__FILE__)."/workers/airbrake", 'airbrake.php', $name);
 
 $payload = array('api_key' => AIRBRAKE_API_KEY);
 
