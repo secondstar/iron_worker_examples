953725d7b009043bebd0c83ca0d43aafd17bd792
Travis Reeder <treeder@gmail.com>
2012-04-20 12:15:09 -0700

    should parse ok now and post to room.

--- ruby_ng/chargify_to_campfire_webhook_worker/chargify_to_campfire_webhook_worker.rb
+++ ruby_ng/chargify_to_campfire_webhook_worker/chargify_to_campfire_webhook_worker.rb
@@ -7,16 +7,18 @@ cgi_parsed = CGI::parse(payload)
 puts "cgi_parsed: #{cgi_parsed.inspect}"
 
 # Then we can parse the json
-parsed = JSON.parse(cgi_parsed['payload'][0])
+parsed = JSON.parse(cgi_parsed)
 puts "parsed: #{parsed.inspect}"
+
+@event = parsed["event"]
 
 # Also parse the config we uploaded with this worker for our Hipchat stuff
 webhook_config = YAML.load_file('webhook_config.yml')
 puts "webhook_config: #{webhook_config.inspect}"
 
-#Broach.settings = {
-#    'account' => 'myaccount',
-#    'token'   => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-#    'use_ssl' => true
-#}
-#Broach.speak('Office', 'Manfred just deployed a new version of the weblog (http://www.fngtps.com)')
+Broach.settings = {
+    'account' => webhook_config['account'],
+    'token'   => webhook_config['token'],
+    'use_ssl' => true
+}
+Broach.speak(webhook_config['room'], @event)
