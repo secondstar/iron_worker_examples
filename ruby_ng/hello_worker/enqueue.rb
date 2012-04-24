131283833fd85245d3f4e80ba55e0103a9dc4d7a
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-04-24 05:34:19 +0300

    New form of examples.

--- ruby_ng/hello_worker/enqueue.rb
+++ ruby_ng/hello_worker/enqueue.rb
@@ -1,10 +1,8 @@
 require 'iron_worker_ng'
-require_relative '../../ruby/examples_helper'
-
-config = ExamplesHelper.load_config
-
-client = IronWorkerNG::Client.new(:token => config['iw']['token'], :project_id => config['iw']['project_id'])
+client = IronWorkerNG::Client.new
 
 # let's queue it now
 # note that code package name was guessed from worker name
 client.tasks.create('HelloWorker', 'some_param' => 'some_value', 'other_param' => [1, 2, 3])
+
+puts "Now you check http://hud.iron.io to see your task status and log"
