cbdf9b6cd6412de0bbcced54e646506e5c735b7f
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-04-28 23:45:56 +0300

    New configuration scheme.

--- ruby_ng/master_slave/enqueue.rb
+++ ruby_ng/master_slave/enqueue.rb
@@ -1,13 +1,10 @@
 require 'iron_worker_ng'
-require_relative '../../ruby/examples_helper'
 
-config = ExamplesHelper.load_config
-
-client = IronWorkerNG::Client.new(:token => config['iw']['token'], :project_id => config['iw']['project_id'])
+client = IronWorkerNG::Client.new
 
 slaves = {
   'slave one' => {'foo' => 'bar'},
   'slave two' => {'hello' => 'world'}
 }
 
-client.tasks.create('MasterWorker', 'slaves' => slaves)
+client.tasks.create('MasterWorker', 'slaves' => slaves, 'token' => client.api.token, 'project_id' => client.api.project_id)
--- ruby_ng/master_slave/master_worker.rb
+++ ruby_ng/master_slave/master_worker.rb
@@ -1,6 +1,6 @@
 require 'iron_worker_ng'
 
-client = IronWorkerNG::Client.new(:token => iron_io_token, :project_id => iron_io_project_id) # they are magically available inside worker
+client = IronWorkerNG::Client.new(:token => params['token'], :project_id => params['project_id'])
 
 puts "Queueing slave workers..."
 slaves = params['slaves']
