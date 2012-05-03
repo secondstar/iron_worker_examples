80ed0ca84c75ce23370c6071926d1937cf6aa777
Travis Reeder <treeder@gmail.com>
2012-05-02 18:17:26 -0700

    Using new worker file format.

--- ruby_ng/hello_worker/README.md
+++ ruby_ng/hello_worker/README.md
@@ -2,8 +2,7 @@
 
 This is the simplest example you can find and is a great place to start. It simply logs some output to the worker log.
 
-1. Be sure you've done your one time configuration
-2. Run `iron_worker codes.create -n HelloWorker`
+1. Be sure you've setup your .iron.json file.
+2. Run `iron_worker codes.create -n hello`
 3. Run `ruby enqueue.rb`
 4. Look at [HUD](https://hud.iron.io) to view your tasks running, check logs, etc.
-
--- ruby_ng/hello_worker/enqueue.rb
+++ ruby_ng/hello_worker/enqueue.rb
@@ -4,6 +4,12 @@ require 'iron_worker_ng'
 client = IronWorkerNG::Client.new
 
 # Now create/queue a task for the worker
-client.tasks.create('HelloWorker', 'some_param' => 'some_value', 'other_param' => [1, 2, 3])
+task = client.tasks.create('hello', 'some_param' => 'some_value', 'other_param' => [1, 2, 3])
 
-puts "Your task has been queued up, check https://hud.iron.io to see your task status and log."
+puts "Your task has been queued up, check https://hud.iron.io to see your task status and log or wait for it below..."
+
+result = client.tasks.wait_for(task.id)
+p result
+
+log = client.tasks.log(task.id)
+puts log
