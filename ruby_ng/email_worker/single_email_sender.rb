656432b8c8132c6bd555b407a9ca1a63906ea5db
rkononov <rkononov@gmail.com>
2012-06-05 22:55:04 +0600

    [#45]Refactor Email Example (master/slave, IronMQ, _ng, php)

--- ruby_ng/email_worker/single_email_sender.rb
+++ ruby_ng/email_worker/single_email_sender.rb
@@ -1,9 +1,10 @@
 require 'yaml'
 require 'iron_worker_ng'
-# Create an IronWorker client
 
+# loading config from config file
 config_data = YAML.load_file '../../ruby/_config.yml'
 
+# Create an IronWorker client
 client = IronWorkerNG::Client.new(:token => config_data['iw']['token'], :project_id => config_data['iw']['project_id'])
 
 email = config_data['email']
@@ -11,9 +12,10 @@ params = {:username => email['username'],
           :password => email['password'],
           :domain => 'simpleworker.com',
           :from => email['from'],
-          :to => email['to'],
+          :to => [email['to'],email['to']],
           :subject => 'sample',
           :content => 'HEY ITs a body',
           :provider => 'gmail'
 }
+#launching worker
 client.tasks.create("email_worker", params)
