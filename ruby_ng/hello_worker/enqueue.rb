cda955c3f8b0bd508fd8d977f7ae0cb333ebd300
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-06-10 01:18:10 +0300

    Fixing NG hello worker.

--- ruby_ng/hello_worker/enqueue.rb
+++ ruby_ng/hello_worker/enqueue.rb
@@ -4,7 +4,7 @@ require 'iron_worker_ng'
 client = IronWorkerNG::Client.new
 
 # Now create/queue a task for the worker
-task = client.tasks.create('hello', 'some_param' => 'some_value', 'other_param' => [1, 2, 3])
+task = client.tasks.create('RubyHelloWorker', 'some_param' => 'some_value', 'other_param' => [1, 2, 3])
 
 puts "Your task has been queued up, check https://hud.iron.io to see your task status and log or wait for it below..."
 
