cb07270ecfafbfdcd1ecae350434b0eeabb10216
thousandsofthem <thousandsofthem@gmail.com>
2012-07-10 23:54:25 +0300

    upload target image to imgur

--- binary/phantom-nodejs/nearme-slave.js
+++ binary/phantom-nodejs/nearme-slave.js
@@ -1,5 +1,7 @@
 var phantom = require('phantom');
 var fs = require('fs');
+var http = require('http');
+var querystring = require('querystring');
 
 var payloadIndex = -1;
 process.argv.forEach(function(val, index, array) {
@@ -18,7 +20,7 @@ if (!url){
 /*
  * Render page to .png image file
  */
-var output = 'screenshot.png';
+var output = __dirname + '/screenshot.png';
 
 phantom.create(function(ph) {
   return ph.createPage(function(page) {
@@ -29,8 +31,47 @@ phantom.create(function(ph) {
       } else {
         page.render(output);
         console.log("page rendered to " + output);
+        setTimeout(function () {
+          upload_file(output);
+        }, 500);
       }
       ph.exit();
     });
   });
 });
+
+
+var upload_file = function(file_name){
+  var file_data = fs.readFileSync(file_name);
+  var post_data = querystring.stringify({
+    'key' : 'd4864bbac09661b4722f6f02ec2e5146',  // api key
+    'image': new Buffer(file_data).toString('base64')
+  });
+  var post_options = {
+    host: 'api.imgur.com',
+    port: '80',
+    path: '/2/upload.json',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_data.length
+    }
+  };
+  var post_req = http.request(post_options, function(pres) {
+    pres.setEncoding('utf8');
+    var response = "";  // this will be the response to the POST
+    pres.on('data', function (chunk) {
+      response += chunk;
+    });
+    pres.on('end', function() {
+      console.log('Response: ' + response);
+      var obj = JSON.parse(response); // imgur answers with json
+      var result = obj.upload.links;
+      console.dir(result); // this will contain the imgur links (image, imgur page, delete, etc.)
+    });
+  });
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+};
+
