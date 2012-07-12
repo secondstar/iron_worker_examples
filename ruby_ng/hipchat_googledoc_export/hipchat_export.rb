5ff6cf81dabeb08e587dbbfa24fdcbac9134ccfb
Travis Reeder <treeder@gmail.com>
2012-07-12 12:16:20 -0700

    Updated to use oauth2 token and other minor changes.

--- ruby_ng/hipchat_googledoc_export/hipchat_export.rb
+++ ruby_ng/hipchat_googledoc_export/hipchat_export.rb
@@ -1,27 +1,47 @@
 require 'active_support/core_ext'
 require 'hipchat-api'
 require 'google_drive'
+require 'oauth2'
 
 ####################
 #PARAMS
 puts params.inspect
 hipchat_token = params["hipchat_token"]
+exclude_rooms = params["exclude_rooms"] || []
+# Need either google username and pass or an oauth2 token
+google_user = params["google_username"]
 google_pass = params["google_password"]
-google_user = params["google_usename"]
+# OR
+google_client_id = params["google_client_id"]
+google_secret = params["google_secret"]
+google_refresh_token = params["google_refresh_token"]
 range_ago = params["range_ago"]
 ###################
 
 
 range_ago||=1
-session = GoogleDrive.login(google_user, google_pass)
+if google_refresh_token
+  client = OAuth2::Client.new(
+      google_client_id,
+      google_secret,
+      :site => "https://accounts.google.com",
+      :token_url => "/o/oauth2/token",
+      :authorize_url => "/o/oauth2/auth")
+  access_token = OAuth2::AccessToken.from_hash(client, {:refresh_token => google_refresh_token})
+  access_token = access_token.refresh!
+  session = GoogleDrive.login_with_oauth(access_token)
+else
+  session = GoogleDrive.login(google_user, google_pass)
+end
 hipchat = HipChat::API.new(hipchat_token)
 end_date = Time.now
 start_date = end_date - range_ago.month
 rooms = hipchat.rooms_list
+p rooms
 open_rooms = rooms.to_hash["rooms"].select { |r| !r["is_private"] }
 rooms_history = {}
 
-filename = Time.now.strftime('%Y-%m')
+filename = "HipChat Archive - #{Time.now.strftime('%Y-%m')}"
 file = session.files("title" => filename)
 
 puts "Looking for file: #{filename}"
@@ -35,15 +55,20 @@ end
 
 
 open_rooms.each do |room|
-  ws = file.worksheet_by_title(room["name"])
+  room_name = room["name"]
+  puts "Looking for a worksheet: #{room_name}"
+  if exclude_rooms.include?(room_name)
+    puts "Skipping room."
+    next
+  end
+  ws = file.worksheet_by_title(room_name)
   import_from = Time.at(room["created"])
-  puts "Looking for a worksheet: #{room["name"]}"
   if ws
     puts "Worksheet found!"
     import_from = Time.parse(ws.list.entries.last["date"]) if ws.list.entries.last
   else
     puts "No such worksheet, creating new"
-    ws = file.add_worksheet(room["name"])
+    ws = file.add_worksheet(room_name)
     ws.list.keys = ["date", "from", "message"]
     ws.save
   end
@@ -52,16 +77,21 @@ open_rooms.each do |room|
   days_back = ((end_date - import_from)/60/60/24).ceil
   puts "Should look for #{days_back} days back"
   days_back.downto(0) do |i|
-    formatted_date = (end_date - i.days).strftime('%y-%m-%d')
+    date_to_get = (end_date - i.days)
+    formatted_date = date_to_get.strftime('%Y-%m-%d')
     puts "Getting messages #{i} days ago - #{formatted_date}"
-    messages = rooms_history[room["name"]]= hipchat.rooms_history(room["name"], formatted_date, 'UTC').parsed_response["messages"]
+    messages = rooms_history[room_name]= hipchat.rooms_history(room_name, formatted_date, 'UTC').parsed_response["messages"]
     puts "Found messages:#{messages.inspect}"
-    if messages
-      messages.each do |m|
+    if messages && messages.size > 0
+      messages.each_with_index do |m,i|
         next if Time.parse(m["date"]) <= import_from
         puts "Pushing message :#{m.inspect}"
         ws.list.push({:date => m["date"], :from => m["from"]["name"], :message => m["message"]})
+        ws.save if i % 50 == 0
       end
+      ws.save
+    else
+      ws.list.push({:date => date_to_get.strftime("%m/%d/%Y"), :from => "--", :message => "No messages"})
       ws.save
     end
   end
--- ruby_ng/hipchat_googledoc_export/run_hipchat_export.rb
+++ ruby_ng/hipchat_googledoc_export/run_hipchat_export.rb
@@ -1,14 +1,13 @@
 require 'iron_worker_ng'
+require 'yaml'
 
-IronCore::Logger.logger.level = ::Logger::DEBUG
+#IronCore::Logger.logger.level = ::Logger::DEBUG
 
 # Create IronWorker client
 client = IronWorkerNG::Client.new
 
-# Now create/queue a task for the worker
-params = {"hipchat_token" => "YOURTOKEN",
-          "google_password" => "YOURPASSWORD",
-          "google_username" => "YOURUSERNAME"}
+params = YAML.load_file("config.yml")
+
 task = client.tasks.create('HipchatExport', params)
 
 puts "Your task has been queued up, check https://hud.iron.io to see your task status and log or wait for it below..."
