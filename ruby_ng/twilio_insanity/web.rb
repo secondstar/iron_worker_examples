3eedae32f462d0625b61599611f40a4b612e0d18
Chad Arimura <carimura@gmail.com>
2012-07-13 17:56:40 -0700

    adding initial twilio insanity reminder add

---
+++ ruby_ng/twilio_insanity/web.rb
@@ -0,0 +1,66 @@
+require 'sinatra'
+require 'iron_cache'
+require 'iron_worker_ng'
+require 'twilio-ruby'
+use Rack::Logger
+
+helpers do
+  def puts(msg)
+    request.logger.info msg
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/start' do
+  number = params[:number]
+  cache = get_cache("insanity-#{number}")
+  load_schedule(cache)
+  cache.put("day", 0)
+
+  config = YAML.load_file("config/config.yml")
+  client = IronWorkerNG::Client.new(:token => config['iron']['token'], :project_id => config['iron']['project_id'])
+
+  client.schedules.create('SendInsanity',
+                          {
+                            :number => number
+                          },
+                          {
+                            :start_at => Time.now,
+                            :run_times => 5,
+                            :run_every => 60
+                          })
+
+  redirect '/done'
+end
+
+get '/done' do
+  erb :done
+end
+
+
+
+private
+
+
+def get_cache(name)
+  puts "Creating or Getting Cache...."
+  ironcache = IronCache::Client.new
+  ironcache.cache(name)
+end
+
+
+def load_schedule(cache)
+  puts "Loading Cache Up...."
+  i=0
+  File.open('lists/insanity_schedule.txt', 'r') do |f|
+    while line = f.gets
+      cache.put(i.to_s, line)
+      i+=1
+    end
+  end
+end
+
+
