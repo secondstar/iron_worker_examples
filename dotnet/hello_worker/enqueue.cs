ad4057a03e8175ca9d30e821b570607f543db24c
Evan Shaw <edsrzf@gmail.com>
2012-07-25 12:21:44 +1200

    Fix up enqueue.cs

--- dotnet/hello_worker/enqueue.cs
+++ dotnet/hello_worker/enqueue.cs
@@ -7,40 +7,36 @@ using System.Text;
 using System.Web;
 
 abstract
-public class EnQueueTask
+public class EnqueueTask
 {
-	static public void Main (string[] args)
-	{
-string project_id = "YOUR_PROJECT_ID";
-string token = "YOUR_TOKEN";
-string url = "http://worker-aws-us-east-1.iron.io/2/projects/" + project_id + "/tasks";
-HttpWebRequest req = WebRequest.Create(new Uri(url))
-                     as HttpWebRequest;
-req.Method = "POST";
-req.ContentType = "application/json";
-//setting oauth authorization
-req.Headers.Add("Authorization", "OAuth " + token);
-string paramz = "{\"tasks\":[{\"code_name\":\"hello\",\"payload\":\"{'query':'Heyyaa'}\"},\"delay\":\"5\"]}";
+    static public void Main (string[] args)
+    {
+        string project_id = "YOUR_PROJECT_ID";
+        string token = "YOUR_TOKEN";
+        string url = "http://worker-aws-us-east-1.iron.io/2/projects/" + project_id + "/tasks";
+        var req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
+        req.Method = "POST";
+        req.ContentType = "application/json";
+        //setting oauth authorization
+        req.Headers.Add("Authorization", "OAuth " + token);
+        var paramz = "{\"tasks\":[{\"code_name\":\"hello\",\"payload\":\"{'query':'Heyyaa'}\"}],\"delay\":\"5\"}";
 
-// Encode the parameters as form data:
-byte[] formData =
-    UTF8Encoding.UTF8.GetBytes(paramz);
-req.ContentLength = formData.Length;
+        // Encode the parameters as form data:
+        var formData = UTF8Encoding.UTF8.GetBytes(paramz);
+        req.ContentLength = formData.Length;
 
-// Send the request:
-using (Stream post = req.GetRequestStream())
-{
-  post.Write(formData, 0, formData.Length);
-}
+        // Send the request:
+        using (var post = req.GetRequestStream())
+        {
+            post.Write(formData, 0, formData.Length);
+        }
 
-// Pick up the response:
-string result = null;
-using (HttpWebResponse resp = req.GetResponse()
-                              as HttpWebResponse)
-{
-  StreamReader reader =
-      new StreamReader(resp.GetResponseStream());
-  result = reader.ReadToEnd();
-}
-}
+        // Pick up the response:
+        string result = null;
+        using (var resp = req.GetResponse() as HttpWebResponse)
+        {
+            StreamReader reader = new StreamReader(resp.GetResponseStream());
+            result = reader.ReadToEnd();
+        }
+    }
 }
