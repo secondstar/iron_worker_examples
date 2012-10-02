8534304767a44befe08bbad7ab579e628ff3475d
rkononov <rkononov@gmail.com>
2012-10-02 21:25:27 +0600

    added remote performance test

--- ruby_ng/image_processing/local_performance_tests.rb
+++ ruby_ng/image_processing/local_performance_tests.rb
@@ -1,20 +1,19 @@
-ranges = [1, 10, 100, 1000]
+ranges = [1, 10, 100]
 
 def params
   {'disable_network' => true}
 end
-
 times = {}
 ranges.each do |r|
   system "rm #{r}.csv"
-  system "dstat -c -d -m --output #{r}.csv &"
+  pipe = IO.popen("dstat -c -d -m --output #{r}.csv &")
   start_time = Time.now
   r.times do |i|
     puts "Launching:#{i}"
     fork { load 'image_processor.rb' }
   end
   Process.waitall
-  system('killall -9 python')
+  system("kill -9 #{pipe.pid}")
   puts "Processing time = #{Time.now - start_time}"
   times[r] = Time.now - start_time
 end
