82e02cfc1eff341c6a27e1c33b2cc917fb17c2ef
Paddy Foran <foran.paddy@gmail.com>
2012-12-29 02:47:30 -0500

    Updated Airbrake example to new syntax.

--- python/Airbrake.py
+++ python/Airbrake.py
@@ -1,17 +1,15 @@
 from iron_worker import *
 
-import airbrakepy
-import shutil
+worker = IronWorker()
 
-worker = IronWorker(token='XXXXXXXXXX', project_id='xxxxxxxxxxx')
+package = CodePackage()
+package.name = "AirbrakeExample"
+package.merge("airbrake", ignoreRootDir=True)
+package.merge_dependency("airbrakepy")
+package.merge_dependency("xmlbuilder")
+package.executable = "worker.py"
 
-#here we have to include AirbrakePy library with worker.
-worker_dir = os.path.dirname(__file__) + '/airbrake'
-abrakepy_dir = os.path.dirname(airbrakepy.__file__)
-shutil.copytree(abrakepy_dir, worker_dir + '/airbrakepy') #copy it to worker directory
+worker.upload(package)
 
-IronWorker.zipDirectory(directory="airbrake/", destination='worker.zip', overwrite=True)
-
-res = worker.postCode(runFilename='worker.py', zipFilename='worker.zip', name='Airbrake.py sample')
-
-task = worker.postTask(name='Airbrake.py sample')
+task = worker.queue(code_name='AirbrakeExample')
+print task
