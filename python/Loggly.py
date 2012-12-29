0f26cd2189837a48a5ea714f76267b5ed5822d83
Paddy Foran <foran.paddy@gmail.com>
2012-12-29 02:49:05 -0500

    updated loggly example to new api.

--- python/Loggly.py
+++ python/Loggly.py
@@ -1,18 +1,16 @@
 from iron_worker import *
-import shutil
 
-import hoover
+worker = IronWorker()
 
-#here we have to include hoover library with worker.
-worker_dir = os.path.dirname(__file__) + '/loggly'
-hoover_dir = os.path.dirname(hoover.__file__)
-shutil.copytree(hoover_dir, worker_dir + '/loggly') #copy it to worker directory
+package = CodePackage()
+package.name = "LogglyExample"
+package.merge("loggly", ignoreRootDir=True)
+package.merge_dependency("hoover")
+package.merge_dependency("httplib2")
+package.executable = "loggly.py"
 
-worker = IronWorker(config='config.ini')
-IronWorker.zipDirectory(directory=worker_dir, destination='loggly-py.zip', overwrite=True)
-
-res = worker.postCode(runFilename='loggly.py', zipFilename='loggly-py.zip', name='loggly-py')
-
+worker.upload(package)
 payload = {'loggly': {'subdomain': 'LOGGLY_SUBDOMAIN', 'username': 'LOGGLY_USERNAME', 'password': 'LOGGLY_PASSWORD'}}
 
-task = worker.postTask(name='loggly-py')
+task = worker.queue(code_name='LogglyExample', payload=payload)
+print task
--- python/loggly/loggly.py
+++ python/loggly/loggly.py
@@ -1,20 +1,25 @@
 import hoover
 import logging
-
-import argparse
+import sys
 import json
 
-parser = argparse.ArgumentParser(
-        description="Some stuff")
-parser.add_argument("-payload", type=str, required=False,
-        help="The location of a file containing a JSON payload.")
-args = parser.parse_args()
-if args.payload is not None:
-    payload = json.loads(open(args.payload).read())
-    if 'loggly' in payload:
-        loggly_settings = payload['loggly']
-        i = hoover.LogglySession(loggly_settings['subdomain'], loggly_settings['username'], loggly_settings['password'])
-        i.config_inputs() #inject loggly handler into logger chain
+payload = None
+payload_file = None
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "-payload" and (i + 1) < len(sys.argv):
+        payload_file = sys.argv[i + 1]
+        break
+
+f = open(payload_file, "r")
+contents = f.read()
+f.close()
+
+payload = json.loads(contents)
+
+if 'loggly' in payload:
+    loggly_settings = payload['loggly']
+    i = hoover.LogglySession(loggly_settings['subdomain'], loggly_settings['username'], loggly_settings['password'])
+    i.config_inputs() #inject loggly handler into logger chain
 
 #and then usual yada-yada is going on
 logger = logging.getLogger('worker_log')
